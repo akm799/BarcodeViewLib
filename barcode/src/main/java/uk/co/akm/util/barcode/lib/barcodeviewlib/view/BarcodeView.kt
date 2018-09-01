25543a2c81bc6e00b8c65a3b78b24b90284f4193
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2018-09-01 21:35:50 +0100

    Added getters and setters for all properties set by attributes.

--- barcode/src/main/java/uk/co/akm/util/barcode/lib/barcodeviewlib/view/BarcodeView.kt
+++ barcode/src/main/java/uk/co/akm/util/barcode/lib/barcodeviewlib/view/BarcodeView.kt
@@ -24,9 +24,27 @@ class BarcodeView(context: Context, attrs: AttributeSet?, defStyleAttr: Int, def
     private val defaultWidthOverHeightRatio = 6f
     private val defaultBarColour = Color.BLACK
 
+    /**
+     * Underlying barcode text.
+     */
     var text: String?
         get() = textValue
         set(value) = setTextValue(value)
+
+    /**
+     * Colour of the barcode bars.
+     */
+    var barColour: Int
+        get() = barColourValue
+        set(value) { barColourValue = value }
+
+    /**
+     * The ratio of the width of the barcode over its height. Please note that this property will
+     * not be used if the dimensions of this view are set to a fixed value.
+     */
+    var widthOverHeightRation: Float
+        get() = widthOverHeightRatioValue
+        set(value) { setWidthOverHeightRatioValue(value) }
 
     private var textValue: String? = null
     private var barColourValue = defaultBarColour
@@ -56,8 +74,30 @@ class BarcodeView(context: Context, attrs: AttributeSet?, defStyleAttr: Int, def
         barPaint.color = barColourValue
     }
 
+    /**
+     * Returns the width of a bar in pixels that has been set for this view. Please not that this
+     * width has no effect if the dimensions of this view are set to a fixed value.
+     */
+    fun getModuleWidthPx(): Int = moduleWidthPxValue
+
+    /**
+     * Sets the with of a bar in DIP. Please note that this method has no effect if the dimensions
+     * of this view are set to a fixed value.
+     */
+    fun setModuleWidthDp(value: Int) {
+        if (value >= 0) {
+            moduleWidthPxValue = dpToPx(value)
+        }
+    }
+
+    private fun setWidthOverHeightRatioValue(value: Float) {
+        if (value > 0) {
+            widthOverHeightRatioValue = value
+        }
+    }
+
     private fun correctDimensionValues() {
-        if (moduleWidthPxValue == 0) {
+        if (moduleWidthPxValue <= 0) {
             moduleWidthPxValue = dpToPx(defaultModuleWidthDp)
         }
 
@@ -91,7 +131,20 @@ class BarcodeView(context: Context, attrs: AttributeSet?, defStyleAttr: Int, def
         }
     }
 
-    // https://stackoverflow.com/questions/12266899/onmeasure-custom-view-explanation
+    /**
+     * If the dimensions of this view are fixed, then the width of the bar in the barcode will be
+     * set in such a way so as the barcode view fits in this fixed width. The height of the barcode
+     * will fit the fixed height. If the undelying barcode text results in a barcode that is to wide
+     * to fit in the fixed width, then no barcode will be shown.
+     *
+     * If the dimensions of this view are free (wrap_content), then the width of a bar in the barcode
+     * is set by the user via the corresponding attribute or setter method. The default bar width is
+     * 2 DIP. The height the barcode is set wrt the width by setting the width-to-height-ratio property.
+     * This property is set by the corresponding attribute or setter method and has a default value
+     * of 6.
+     *
+     * https://stackoverflow.com/questions/12266899/onmeasure-custom-view-explanation
+     */
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val desiredWidth = computeDesiredWidth()
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
